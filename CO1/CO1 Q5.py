b2bcd8b80cb4613a44b69fe41e2b823dcf0d32a3
Himavath Nyalakonda <nyalakondahimavath99@gmail.com>
2026-07-23 13:57:13 +0530

    Add files via upload

---
+++ CO1/CO1 Q5.py
@@ -0,0 +1,12 @@
+import numpy
+import pandas
+import matplotlib
+import sklearn
+import torch
+
+
+print("Numpy:", numpy.__version__)
+print("Pandas:", pandas.__version__)
+print("Matplotlib:", matplotlib.__version__)
+print("Scikit-learn:", sklearn.__version__)
+print("Pytorch:" , torch.__version__)
---
+++ CO1/CO1.Q4.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+data = {
+    "Name": ["Isha", "Nikhil" , "Arun" , "Varun" , "Xebac"],
+    "Grade": ["A" , "A+" , "C" , "B" , "S"]
+} 
+df = pd.DataFrame(data)
+print(df)
